a62fb1c3a55e5a40eecc060631e0e1da880a2bc4
Rafael Matthew Susanto Kwan <78295470+rafaelmtw@users.noreply.github.com>
2021-04-04 02:25:02 +0800

    Create snake.cpp
    draw function and some headers

---
+++ snake.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+const int height = 20;
+const int width = 20;
+const char border = 178;
+
+class Position {
+public:
+  int x;
+  int y;
+  int fruitx;
+  int fruity;
+};
+
+enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
+Direction dir;
+
+void Draw()
+{
+  system("cls");
+  for (int i = 0; i < width; i++) {
+    cout << border;
+  }
+  cout << endl;
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      if (j == 0) {
+        cout << border;
+      }
+      if (i == x && j == y) {
+        cout << "O";
+      }
+      if (j == width - 1) {
+        cout << border;
+      }
+    }
+  }
+}
+  
