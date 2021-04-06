b059f9f8b8db99e86a5da884a4f8588603adf4c9
Rafael Matthew Susanto Kwan <78295470+rafaelmtw@users.noreply.github.com>
2021-04-06 15:27:09 +0800

    Update snake.cpp
    startposition function and userinterface function

--- snake.cpp
+++ snake.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <stdio.h>
+#include <ctime>
 using namespace std;
 
 const int height = 20;
 const int width = 20;
-const char border = 178;
+const char barrier = 178;
+bool gameOver;
 
 class position {
 public:
@@ -13,30 +16,57 @@ public:
   int y;
   int fruitx;
   int fruity;
+  int score;
 };
-
+position current;
 enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
 Direction dir;
 
-void Draw()
+// a function to setup the game
+void startPosition()
+{
+  srand(time(NULL));                // randomized seed
+  gameOver = false;
+  dir = STOP;
+  current.x = width / 2;
+  current.y = height / 2;
+  current.fruitx = rand() % width;
+  current.fruity = rand() % height;
+  current.score = 0;
+}
+
+// a function to draw the interface
+void userInterface()
 {
   system("cls");
   for (int i = 0; i < width; i++) {
-    cout << border;
+    cout << barrier;
   }
   cout << endl;
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
       if (j == 0) {
-        cout << border;
+        cout << barrier;
       }
-      if (i == position.x && j == position.y) {
+      else if (i == current.x && j == current.y) {
         cout << "O";
       }
-      if (j == width - 1) {
-        cout << border;
+      else if (j == width - 1) {
+        cout << barrier;
+      }
+      else {
+            cout << " ";
       }
     }
+    cout << endl;
+  }
+  for (int k = 0; k < width; k++) {
+    cout << barrier;
   }
 }
-  
+
+int main()
+{
+  startPosition();
+  userInterface();
+}
