ad293732ad8545a55425c51fad53a38f9ea1d74d
Rafael Matthew Susanto Kwan <78295470+rafaelmtw@users.noreply.github.com>
2021-04-16 22:33:18 +0800

    Separate gameplay logic related function
    function files

---
+++ includes/gameplayLogic.h
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class position {
+public:
+  int x;
+  int y;
+  int fruitx;
+  int fruity;
+  int score;
+};
+
+void startPosition();
+
+void userInterface();
+
+void userInput(char z);
+
+void gameplay();
